2a460bdff0a7967665452b3a30a70f1901f8e05a
Constantin Egloff <constantinegloff03@gmail.com>
2023-05-17 16:08:09 +0200

    SnapFace

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'snapface';
+export class AppComponent implements OnInit {
+
+  interval$!: Observable<number>
+
+  ngOnInit() {
+    
+  }
 }
+
